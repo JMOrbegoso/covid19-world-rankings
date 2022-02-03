37c151f640b75c35f45a32e5a9d7d1978052538e
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-02-03 10:13:52 -0500

    Update CountryPage component

--- src/components/CountryPage/CountryPage.tsx
+++ src/components/CountryPage/CountryPage.tsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
 import { Country } from '../../entities';
-import { getCountry, getCountryHistory } from '../../services';
+import { ApiResponse, getCountry, getCountryHistory } from '../../services';
 import {
 	InfoTable,
 	LineChart,
 	LineChartData,
 	LoadingPage,
 	PageHeader,
+	PageNotFound,
 } from '../../components';
 import './CountryPage.css';
 
@@ -16,7 +17,7 @@ export function CountryPage(): JSX.Element {
 	const params = useParams();
 	const countryName = params.countryName;
 
-	const [country, setCountry] = useState<Country>();
+	const [country, setCountry] = useState<ApiResponse<Country>>();
 	const [countryHistoryChartData, setCountryHistoryChartData] =
 		useState<LineChartData>();
 
@@ -28,19 +29,27 @@ export function CountryPage(): JSX.Element {
 			setCountry(country);
 		});
 		getCountryHistory(countryName).then((countryHistory) => {
+			if (!countryHistory.isValid || !countryHistory.data) {
+				return;
+			}
+
 			setCountryHistoryChartData({
-				labels: countryHistory.getHistory.map((history) =>
+				labels: countryHistory.data.getHistory.map((history) =>
 					history.date.toDateString(),
 				),
 				datasets: [
 					{
 						label: 'Cases',
-						values: countryHistory.getHistory.map((history) => history.cases),
+						values: countryHistory.data.getHistory.map(
+							(history) => history.cases,
+						),
 						color: '#FF0000',
 					},
 					{
 						label: 'Deaths',
-						values: countryHistory.getHistory.map((history) => history.deaths),
+						values: countryHistory.data.getHistory.map(
+							(history) => history.deaths,
+						),
 						color: '#000000',
 					},
 				],
@@ -49,70 +58,74 @@ export function CountryPage(): JSX.Element {
 	}, [countryName]);
 
 	return country ? (
-		<div className="page">
-			<Helmet>
-				<title>{`COVID-19 Rankings | ${country.getName}`}</title>
-			</Helmet>
+		country.isValid && country.data ? (
+			<div className="page">
+				<Helmet>
+					<title>{`COVID-19 Rankings | ${country.data.getName}`}</title>
+				</Helmet>
 
-			<div className="page-section">
-				<img
-					className="country-flag"
-					alt={`${country.getName} flag`}
-					src={country.getFlag}
-				/>
-			</div>
-
-			<div className="page-section">
-				<PageHeader title={country.getName} />
-			</div>
-
-			<div className="page-section">
-				<InfoTable
-					data={[
-						{
-							property: 'Population (Inhabitants)',
-							value: country.getPopulation,
-						},
-						{ property: 'Total cases', value: country.getCases },
-						{ property: 'Total deaths', value: country.getDeaths },
-						{ property: 'Total tests', value: country.getTests },
-						{
-							property: 'Cases per 1 million inhabitants',
-							value: country.getCasesPerOneMillion,
-						},
-						{
-							property: 'Deaths per 1 million inhabitants',
-							value: country.getDeathsPerOneMillion,
-						},
-						{
-							property: 'Tests per 1 million inhabitants',
-							value: country.getTestsPerOneMillion,
-						},
-						{
-							property: 'One case per X inhabitants',
-							value: country.getOneCasePerPeople,
-						},
-						{
-							property: 'One death per X inhabitants',
-							value: country.getOneDeathPerPeople,
-						},
-						{
-							property: 'One test per X inhabitants',
-							value: country.getOneTestPerPeople,
-						},
-					]}
-				/>
-			</div>
-
-			<div className="page-section">
-				{countryHistoryChartData && (
-					<LineChart
-						title="COVID-19 Historical Curve"
-						chartData={countryHistoryChartData}
+				<div className="page-section">
+					<img
+						className="country-flag"
+						alt={`${country.data.getName} flag`}
+						src={country.data.getFlag}
 					/>
-				)}
+				</div>
+
+				<div className="page-section">
+					<PageHeader title={country.data.getName} />
+				</div>
+
+				<div className="page-section">
+					<InfoTable
+						data={[
+							{
+								property: 'Population (Inhabitants)',
+								value: country.data.getPopulation,
+							},
+							{ property: 'Total cases', value: country.data.getCases },
+							{ property: 'Total deaths', value: country.data.getDeaths },
+							{ property: 'Total tests', value: country.data.getTests },
+							{
+								property: 'Cases per 1 million inhabitants',
+								value: country.data.getCasesPerOneMillion,
+							},
+							{
+								property: 'Deaths per 1 million inhabitants',
+								value: country.data.getDeathsPerOneMillion,
+							},
+							{
+								property: 'Tests per 1 million inhabitants',
+								value: country.data.getTestsPerOneMillion,
+							},
+							{
+								property: 'One case per X inhabitants',
+								value: country.data.getOneCasePerPeople,
+							},
+							{
+								property: 'One death per X inhabitants',
+								value: country.data.getOneDeathPerPeople,
+							},
+							{
+								property: 'One test per X inhabitants',
+								value: country.data.getOneTestPerPeople,
+							},
+						]}
+					/>
+				</div>
+
+				<div className="page-section">
+					{countryHistoryChartData && (
+						<LineChart
+							title="COVID-19 Historical Curve"
+							chartData={countryHistoryChartData}
+						/>
+					)}
+				</div>
 			</div>
-		</div>
+		) : (
+			<PageNotFound />
+		)
 	) : (
 		<LoadingPage />
 	);
